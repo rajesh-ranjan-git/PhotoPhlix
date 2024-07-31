d21eec1c6754497592f3a81a4d2dc90796255d2d
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-07-31 10:38:33 +0530

    photo api

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PhotoCard from "./PhotoCard";
 
 const Home = () => {
-  return <div>This is Home Component.</div>;
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      setLoading(true);
+      const clientID = "?client_id=AW3Og75vq_jqIfX-snfehkd6iLVRkaqE4tYA2e4gsRo";
+      const mainUrl = "https://api.unsplash.com/photos/";
+
+      try {
+        const response = await fetch(`${mainUrl}${clientID}`);
+        const data = await response.json();
+
+        setPhotos(data);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        console.log(err);
+      }
+    };
+
+    fetchPhotos();
+  }, []);
+
+  return (
+    <main className="mt-28 mx-10">
+      <section className="photos">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          photos.map((photo) => {
+            return <PhotoCard photo={photo} key={photo.id} className="photo" />;
+          })
+        )}
+      </section>
+    </main>
+  );
 };
 
 export default Home;
