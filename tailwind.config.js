ea4afd29831623595a49337f54c00cb733451ccf
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-11-28 18:32:29 +0530

    Fixed flowbite import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "flowbite/plugin";
 export default {
   content: [
     "./index.html",
@@ -8,5 +9,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [plugin],
 };
